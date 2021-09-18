8afea72a1f81ad68e0fd1254fdde06ac1df1e3e1
Алексеев Алексей <Alale20002504@gmail.com>
2021-09-18 09:28:28 +0500

    [+] Добавлены линии секунд, минут и часов

--- TheBestWatch/TheBestWatch/MainPage.xaml.cs
+++ TheBestWatch/TheBestWatch/MainPage.xaml.cs
@@ -1,12 +1,40 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.ComponentModel;
+using System.Timers;
+using Xamarin.Forms;
 
 namespace TheBestWatch
 {
     public partial class MainPage : ContentPage
     {
+        private Timer _timer;
+
+        public string Time { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+
+            InitTimer();
+        }
+
+        private void InitTimer()
+        {
+            _timer = new Timer {Interval = 500};
+            _timer.Elapsed += TimerOnElapsed;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+        }
+
+        private void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            var dateTime = DateTime.Now;
+            Time = dateTime.ToString("T");
+            SecondLine.Rotation = dateTime.Second * 6 - 180;
+            HourLine.Rotation = dateTime.Hour * 30 - 180;
+            MinuteLine.Rotation = dateTime.Minute * 6 - 180;
+            OnPropertyChanged(nameof(Time));
         }
     }
 }
